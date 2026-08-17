555b8e1d1003d8d86dc475ea8731bd4039928a8e
student1232354 <tarasromanenko33@gmail.com>
2026-08-17 09:59:04 +0300

    fourth update with images

--- src/Components/HomePageList.tsx
+++ src/Components/HomePageList.tsx
@@ -195,7 +195,7 @@ export const HomePageList: React.FC<Props> = ({
             <Link to="/phones" className="ShopByCategory__card">
               <div className="ShopByCategory__image-wrapper ShopByCategory__image-wrapper--phones">
                 <img
-                  src={`/img/category-phones.png`}
+                  src={'/img/category-phones.png'}
                   alt="Mobile phones"
                   className="ShopByCategory__image"
                 />
@@ -208,7 +208,7 @@ export const HomePageList: React.FC<Props> = ({
             <Link to="/tablets" className="ShopByCategory__card">
               <div className="ShopByCategory__image-wrapper ShopByCategory__image-wrapper--tablets">
                 <img
-                  src={`/img/category-tablets.png`}
+                  src={'/img/category-tablets.png'}
                   alt="Tablets"
                   className="ShopByCategory__image"
                 />
@@ -221,7 +221,7 @@ export const HomePageList: React.FC<Props> = ({
             <Link to="/accessories" className="ShopByCategory__card">
               <div className="ShopByCategory__image-wrapper ShopByCategory__image-wrapper--accessories">
                 <img
-                  src={`/img/category-accessories.png`}
+                  src={'/img/category-accessories.png'}
                   alt="Accessories"
                   className="ShopByCategory__image--added"
                 />
